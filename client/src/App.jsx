00935d6006ca573849521bf72969c15d470213cd
yulian5kov <yulian5kov@gmail.com>
2023-04-26 13:13:39 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,51 @@
+import {
+	createBrowserRouter,
+	Outlet,
+	RouterProvider,
+} from "react-router-dom";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+	return(
+		<div className="app">
+			<Navbar/>
+			<Outlet/>
+			<Footer/>
+		</div>
+	)
+}
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout/>,
+		children:[
+			{
+				path:"/",
+				element: <Home/>,
+			},
+			{
+				path:"/products/:id",
+				element: <Products/>,
+			},
+			{
+				path:"/product/:id",
+				element: <Product/>,
+			},
+		]
+	}
+]);
+
+const App = () => {
+	return (
+        <div>
+            <RouterProvider router={router}/>
+        </div>
+    )
+};
+
+export default App;
---
+++ client/src/pages/Products/Products.jsx
@@ -0,0 +1,10 @@
+
+const Products = () => {
+  return (
+	<div>
+		Category
+	</div>
+  )
+}
+
+export default Products
