3e84544cae2c7ee2d2ded6e5da5d5bd67d9e14a2
yulian5kov <yulian5kov@gmail.com>
2023-04-26 14:54:15 +0300

    Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,65 @@
+import './Footer.scss'
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-}
+	return (
+		<div className="footer">
+			<div className="top">
+				<div className="item">
+					<h1>Categories</h1>
+					<span>Women</span>
+					<span>Men</span>
+					<span>Shoes</span>
+					<span>Accessories</span>
+					<span>New Arrivals</span>
+				</div>
+				<div className="item">
+					<h1>Links</h1>
+					<span>FAQ</span>
+					<span>Pages</span>
+					<span>Stores</span>
+					<span>Compare</span>
+					<span>Cookies</span>
+				</div>
+				<div className="item">
+					<h1>About</h1>
+					<span>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</span>
+				</div>
+				<div className="item">
+          					<h1>Contact</h1>
+					<span>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea
+						commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu
+						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit
+						anim id est laborum.
+					</span>
+        </div>
+			</div>
+			<div className="bottom">
+        <div className="left">
+          <span className="logo">STORE LOGO</span>
+          <span className="copyright">© Copyright 2023. All rights reserved</span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
