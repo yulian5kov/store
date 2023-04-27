d20cab8641bd9c9d5433d88c4a1ce94242fb55ce
yulian5kov <yulian5kov@gmail.com>
2023-04-27 16:26:45 +0300

    Images loaded

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,45 +1,33 @@
+import { useEffect, useState } from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import axios from "axios"
+
+const REACT_APP_API_TOKEN =
+	"175f16c305184e0a8820acb7a06f6cc38df05124d697d0d416d10fc8b256e042139ce89a359aae9c32c847cd29c03fdc228a70a177aa2f9d02c1ef5c7e57ca9a64408b49b17f0c773d1a979a2b5c756bfc589cb02a8abe38f4dc992ee044c342627ce8e96e7edf60645e79c6729d568079c13b62ba777275461686688c63f989";
+const REACT_APP_API_URL = "http://localhost:1337/api";
+const REACT_APP_UPLOAD_URL = "http://localhost:1337";
 
 const FeaturedProducts = ({ type }) => {
-	const data = [
-		{
-			id: 1,
-			img: "https://img-9gag-fun.9cache.com/photo/aZyoZDV_460s.jpg",
-			img2:"https://keepmeme.com/meme/20211103/trap-card-bitch-hold-on.jpg",
-			title: "Swag",
-			isNew: true,
-			oldPrice: 19,
-			price: 12,
-		},
-		{
-			id: 2,
-			img: "https://img-9gag-fun.9cache.com/photo/aZyoZDV_460s.jpg",
-			img2:"https://keepmeme.com/meme/20211103/trap-card-bitch-hold-on.jpg",
-			title: "Swag",
-			isNew: false,
-			oldPrice: 19,
-			price: 12,
-		},
-		{
-			id: 3,
-			img: "https://img-9gag-fun.9cache.com/photo/aZyoZDV_460s.jpg",
-			img2:"https://keepmeme.com/meme/20211103/trap-card-bitch-hold-on.jpg",
-			title: "Swag",
-			isNew: true,
-			oldPrice: 19,
-			price: 12,
-		},
-		{
-			id: 4,
-			img: "https://img-9gag-fun.9cache.com/photo/aZyoZDV_460s.jpg",
-			img2:"https://keepmeme.com/meme/20211103/trap-card-bitch-hold-on.jpg",
-			title: "Swag",
-			isNew: true,
-			oldPrice: 19,
-			price: 12,
-		},
-	];
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const res = await axios.get(REACT_APP_API_URL+"/products?populate=*", {
+					headers:{
+						Authorization: "Bearer " + REACT_APP_API_TOKEN,
+					},
+				});
+				setData(res.data.data)
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		fetchData()
+	}, []);
+
+	console.log(data)
 
 	return (
 		<div className="featuredProducts">
@@ -53,10 +41,13 @@ const FeaturedProducts = ({ type }) => {
 				</p>
 			</div>
 			<div className="bottom">
-                {data.map(item=>(
-                    <Card item={item} key={item.id}/>
-                ))}
-            </div>
+				{data.map((item) => (
+					<Card
+						item={item}
+						key={item.id}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
