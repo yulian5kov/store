7c1ba489b226f51da618efea8c45f76d97b1ddab
yulian5kov <yulian5kov@gmail.com>
2023-04-27 17:05:18 +0300

    fetching items

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,55 +1,34 @@
-import { useEffect, useState } from "react";
+import React from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
-import axios from "axios"
-
-const REACT_APP_API_TOKEN =
-	"175f16c305184e0a8820acb7a06f6cc38df05124d697d0d416d10fc8b256e042139ce89a359aae9c32c847cd29c03fdc228a70a177aa2f9d02c1ef5c7e57ca9a64408b49b17f0c773d1a979a2b5c756bfc589cb02a8abe38f4dc992ee044c342627ce8e96e7edf60645e79c6729d568079c13b62ba777275461686688c63f989";
-const REACT_APP_API_URL = "http://localhost:1337/api";
-const REACT_APP_UPLOAD_URL = "http://localhost:1337";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-	const [data, setData] = useState([]);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await axios.get(REACT_APP_API_URL+`/products?populate=*&[filters][type][$eq]=${type}`, {
-					headers:{
-						Authorization: "Bearer " + REACT_APP_API_TOKEN,
-					},
-				});
-				setData(res.data.data)
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		fetchData()
-	}, []);
-
-	console.log(data)
-
-	return (
-		<div className="featuredProducts">
-			<div className="top">
-				<h1>{type} products</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-					do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea
-				</p>
-			</div>
-			<div className="bottom">
-				{data.map((item) => (
-					<Card
-						item={item}
-						key={item.id}
-					/>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
+      </div>
+      <div className="bottom">
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedProducts;
